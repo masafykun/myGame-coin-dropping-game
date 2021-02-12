beb8b7956bd85ca6fb1086f6a3424bda2ab866ab
masafy <mail@masafy.org>
2021-02-12 23:58:01 +0900

    myGame-coin-dropping-game
    自作のコインゲーム。マウスのクリックで飛ぶ場所を変えられる！

---
+++ Assets/Sripte/pusher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pusher : MonoBehaviour {
+
+	Vector3 startPosition;
+
+	public float ampltude=2.0f;
+	public float speed=0.5f;
+
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.localPosition;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float z = ampltude * Mathf.Sin (Time.time * speed);
+		transform.localPosition = startPosition + new Vector3 (0, 0, z);
+	}
+}
---
+++ Assets/Sripte/shooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour {
+
+	public GameObject coinpre;
+	public float shotSpeed;
+	public float shotTorque;
+
+	public float baseWidth;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Fire1"))
+			Shot ();
+		
+	}
+
+	Vector3 GetInstantiatePosition(){
+		float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+		return transform.position+new Vector3(x,0,0);
+	}
+
+
+	public void Shot(){
+		GameObject coin = (GameObject)Instantiate (coinpre, GetInstantiatePosition(), Quaternion.identity);
+
+		Rigidbody candyRigidBody = coin.GetComponent<Rigidbody> ();
+		candyRigidBody.AddForce (transform.forward * shotSpeed);
+		candyRigidBody.AddTorque (new Vector3 (0, shotTorque, 0));
+	}
+}
